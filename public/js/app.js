5c158bf540072eee63fe276256f57bc679942eed
Anna Penchansky <a.penchansky@gmail.com>
2017-06-16 18:13:52 +0300

    added a home button that takes you back to the index

--- public/js/app.js
+++ public/js/app.js
@@ -31,3 +31,7 @@ $("a").click(function(event){
 $('#mymodal').on('shown.bs.modal', function(e){
     $('.btn').one('focus', function(e){$(this).blur();});
 });
+
+function goBack(){
+  window.history.back();
+}
