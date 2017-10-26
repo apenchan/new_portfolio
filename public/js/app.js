c397149678bb3264540491bb45c11d9d1e90f763
anna Penchansky <annapenchansky@annas-MacBook-Pro.local>
2017-10-26 17:06:13 +0300

    updated my page with more css, more mobile friendly that is still under construction

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,67 @@
 console.log("Hello world");
 
 var myGreetings = 0;
-carousel();
+
+var arr = [{
+    greeting: "Welcome",
+    image: "welcomeImage1"
+}, {
+    greeting: "ברוכים הבאים",
+    image: "welcomeImage2"
+}, {
+    greeting: "Bienvenue",
+    image: "welcomeImage3"
+}]
+
+var counter = 0;
 
 function carousel() {
-    var x = document.getElementsByClassName("welcome");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
+    document.getElementById("greeting").innerHTML = arr[counter].greeting;
+    document.getElementById("image").className = arr[counter].image
+    counter++;
+    if (counter === 3) {
+        counter = 0;
     }
-    myGreetings++;
-    if (myGreetings > x.length) {myGreetings = 1}
-    x[myGreetings-1].style.display = "block";
-    setTimeout(carousel, 5000);
 }
 
-$("a").click(function(event){
-  if(this.hash){
-    event.preventDefault();
-    var hash = this.hash
-    console.log(hash);
+// function carousel() {
+//     var x = document.getElementsByClassName("welcome");
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";
+//     }
+//     myGreetings++;
+//     if (myGreetings > x.length) {
+//         myGreetings = 1
+//     }
+//     x[myGreetings - 1].style.display = "block";
+// }
 
-    $("body,html").animate({
-      scrollTop : $(hash).offset().top
-    }, 800, function(){
-      window.location.hash = hash;
-    })
-  }
+
+setInterval(carousel, 5000);
+carousel();
+
+
+$("a").click(function(event) {
+    if (this.hash) {
+        event.preventDefault();
+        var hash = this.hash
+        console.log(hash);
+
+        $("body,html").animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function() {
+            window.location.hash = hash;
+        })
+    }
 });
 
-$('#mymodal').on('shown.bs.modal', function(e){
-    $('.btn').one('focus', function(e){$(this).blur();});
+$('#mymodal').on('shown.bs.modal', function(e) {
+    $('.btn').one('focus', function(e) {
+        $(this).blur();
+    });
 });
 
-
+$(window).scroll(function() {
+  var scrolledY = $(window).scrollTop();
+  $('.project1').css('background-position', 'left ' + ((scrolledY)) + 'px');
+});
